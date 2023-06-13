ff3b8e646046128735255cca3262f84ea8b41201
Andrew Milson <andrew.j.milson@gmail.com>
2023-06-13 19:41:46 +1000

    Add GPS contract

---
+++ test/GpsVerifier.t.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.2; // test
+pragma experimental ABIEncoderV2;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/cpu/layoutStarkNet/CpuConstraintPoly.sol";
+import "../src/periodic_columns/PoseidonPoseidonFullRoundKey0Column.sol";
+import "../src/periodic_columns/PoseidonPoseidonFullRoundKey1Column.sol";
+import "../src/periodic_columns/PoseidonPoseidonFullRoundKey2Column.sol";
+import "../src/periodic_columns/PoseidonPoseidonPartialRoundKey0Column.sol";
+import "../src/periodic_columns/PoseidonPoseidonPartialRoundKey1Column.sol";
+import "../src/periodic_columns/EcdsaPointsXColumn.sol";
+import "../src/periodic_columns/EcdsaPointsYColumn.sol";
+import "../src/periodic_columns/PedersenHashPointsXColumn.sol";
+import "../src/periodic_columns/PedersenHashPointsYColumn.sol";
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/cpu/CairoBootloaderProgram.sol";
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/cpu/layoutStarkNet/FriStatementVerifier.sol";
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/cpu/layoutStarkNet/CpuFrilessVerifier.sol";
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/cpu/MemoryPageFactRegistry.sol";
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/FriStatementContract.sol";
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/cpu/layoutStarkNet/CpuOods.sol";
+import "../src/starkex-contracts/evm-verifier/solidity/contracts/gps_starknet/GpsStatementVerifier.sol";
+import "../src/ProofData.sol";
+
+contract StarkNetVerifierTest is Test {
+    // == CPU layout6 verifier ==
+    // https://etherscan.io/address/0xe9664d230490d5a515ef7ef30033d8075a8d0e24#code
+    uint256 numSecurityBits = 96;
+    uint256 minProofOfWorkBits = 30;
+    CpuConstraintPoly public cpuConstraintPoly;
+    PedersenHashPointsXColumn pedersenPointsX;
+    PedersenHashPointsYColumn pedersenPointsY;
+    EcdsaPointsXColumn ecdsaPointsX;
+    EcdsaPointsYColumn ecdsaPointsY;
+    PoseidonPoseidonFullRoundKey0Column poseidonPoseidonFullRoundKey0Column;
+    PoseidonPoseidonFullRoundKey1Column poseidonPoseidonFullRoundKey1Column;
+    PoseidonPoseidonFullRoundKey2Column poseidonPoseidonFullRoundKey2Column;
+    PoseidonPoseidonPartialRoundKey0Column poseidonPoseidonPartialRoundKey0Column;
+    PoseidonPoseidonPartialRoundKey1Column poseidonPoseidonPartialRoundKey1Column;
+    address[] public auxPolynomials;
+    MemoryPageFactRegistry public memoryPageFactRegistry;
+    MerkleStatementContract public merkleStatementContract;
+    FriStatementContract public friStatementContract;
+    FriStatementVerifier public friStatementVerifier;
+    CpuOods cpuOods;
+    CpuFrilessVerifier public cpuFrilessVerifier;
+
+    // == GPS statement verifier ==
+    // https://etherscan.io/address/0x6cB3EE90C50a38A0e4662bB7e7E6e40B91361BF6#code
+    CairoBootloaderProgram public bootloaderProgram;
+    address[] public cairoVerifierContracts;
+    uint256 hashedSupportedCairoVerifiers;
+    uint256 simpleBootloaderProgramHash;
+    GpsStatementVerifier public gpsStatementVerifier;
+
+    function setUp() public {
+        // TODO: find out what the MemoryPageFactRegistry does
+        memoryPageFactRegistry = new MemoryPageFactRegistry();
+        merkleStatementContract = new MerkleStatementContract();
+        friStatementContract = new FriStatementContract();
+        cpuOods = new CpuOods();
+        // TODO: find the coefficients for this polynomial
+        cpuConstraintPoly = new CpuConstraintPoly();
+        pedersenPointsX = new PedersenHashPointsXColumn();
+        pedersenPointsY = new PedersenHashPointsYColumn();
+        ecdsaPointsX = new EcdsaPointsXColumn();
+        ecdsaPointsY = new EcdsaPointsYColumn();
+        poseidonPoseidonFullRoundKey0Column = new PoseidonPoseidonFullRoundKey0Column();
+        poseidonPoseidonFullRoundKey1Column = new PoseidonPoseidonFullRoundKey1Column();
+        poseidonPoseidonFullRoundKey2Column = new PoseidonPoseidonFullRoundKey2Column();
+        poseidonPoseidonPartialRoundKey0Column = new PoseidonPoseidonPartialRoundKey0Column();
+        poseidonPoseidonPartialRoundKey1Column = new PoseidonPoseidonPartialRoundKey1Column();
+        auxPolynomials = [
+            address(cpuConstraintPoly),
+            address(pedersenPointsX),
+            address(pedersenPointsY),
+            address(ecdsaPointsX),
+            address(ecdsaPointsY),
+            address(poseidonPoseidonFullRoundKey0Column),
+            address(poseidonPoseidonFullRoundKey1Column),
+            address(poseidonPoseidonFullRoundKey2Column),
+            address(poseidonPoseidonPartialRoundKey0Column),
+            address(poseidonPoseidonPartialRoundKey1Column)
+        ];
+        cpuFrilessVerifier = new CpuFrilessVerifier(
+            auxPolynomials,
+            address(cpuOods),
+            address(memoryPageFactRegistry),
+            address(merkleStatementContract),
+            address(friStatementContract),
+            numSecurityBits,
+            minProofOfWorkBits
+        );
+
+        bootloaderProgram = new CairoBootloaderProgram();
+        // TODO: what are these? Maybe ask Starkware for the pre-images
+        hashedSupportedCairoVerifiers = 3178097804922730583543126053422762895998573737925004508949311089390705597156;
+        simpleBootloaderProgramHash = 2962621603719000361370283216422448934312521782617806945663080079725495842070;
+        // TODO: in reality these addresses map to different verifiers
+        // For the sake of simplicity have the same amount of verifiers
+        // as the on-chain contract but have them be all the same address
+        cairoVerifierContracts = [
+            address(cpuFrilessVerifier),
+            address(cpuFrilessVerifier),
+            address(cpuFrilessVerifier),
+            address(cpuFrilessVerifier),
+            address(cpuFrilessVerifier),
+            address(cpuFrilessVerifier),
+            address(cpuFrilessVerifier),
+            address(cpuFrilessVerifier)
+        ];
+        gpsStatementVerifier = new GpsStatementVerifier(
+            address(bootloaderProgram),
+            address(memoryPageFactRegistry),
+            cairoVerifierContracts,
+            hashedSupportedCairoVerifiers,
+            simpleBootloaderProgramHash
+        );
+    }
+
+    function testVerify() public {
+        ProofData proofData = new ProofData();
+        uint256 cairoVerifierId = proofData.cairoVerifierId();
+        console.logUint(proofData.cairoVerifierId());
+        console.log(address(bootloaderProgram));
+        gpsStatementVerifier.verifyProofAndRegister(
+            proofData.getProofParams(),
+            proofData.getProof(),
+            proofData.getTaskMetadata(),
+            proofData.getCairoAuxInput(),
+            proofData.cairoVerifierId()
+        );
+    }
+
+    // function testIncrement() public {
+    //     counter.increment();
+    //     assertEq(counter.number(), 1);
+    // }
+
+    // function testSetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
+}
